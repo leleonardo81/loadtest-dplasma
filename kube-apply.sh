60b2f24a866743596d6f1aa72c60e6643d04828c
leonardo81 cloudshell <leonardo81@ui.ac.id>
2021-10-19 16:42:16 +0000

    pull merge

---
+++ kube-apply.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+kubectl apply -f kubernetes-config/locust-master-controller.yaml
+kubectl apply -f kubernetes-config/locust-master-service.yaml
+kubectl apply -f kubernetes-config/locust-worker-controller.yaml
