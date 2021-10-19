5e7fe9d67a26e1e8f08088f35546f8769db5a87d
Leo :) <leonardo.leonardo@ibm-jti.com>
2021-10-20 00:04:42 +0700

    fix buuildimage

--- build-image.sh
+++ build-image.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-gcloud builds submit --tag gcr.io/$PROJECT/locust-tasks:latest docker-image
+PROJECT=$(gcloud config get-value project)
+gcloud builds submit \
+    --tag gcr.io/$PROJECT/locust-tasks:latest docker-image
 gcloud container images list | grep locust-tasks
